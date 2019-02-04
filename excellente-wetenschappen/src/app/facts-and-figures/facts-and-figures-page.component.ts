127811d2a725eb00f282bcf518b2eff2e25bdb55
david-bulte <david.bulte@gmail.com>
2019-02-04 12:16:32 +0100

    add facts-and-figures movie

--- excellente-wetenschappen/src/app/facts-and-figures/facts-and-figures-page.component.ts
+++ excellente-wetenschappen/src/app/facts-and-figures/facts-and-figures-page.component.ts
@@ -4,10 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-facts-and-figures',
   template: `
     <div class="d-flex flex-column align-items-center w-100 position-relative">
-      <div class="text-container">
-        coming soon
+
+      <div class="text-container mb-3">
+        <iframe class="d-none d-md-block" width="560" height="315" 
+                src="https://www.youtube.com/embed/d9ethx20c-Y" 
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        <div class="d-block d-md-none embed-responsive embed-responsive-4by3">
+          <iframe class="embed-responsive-item" width="560" height="315"
+                  src="https://www.youtube.com/embed/d9ethx20c-Y"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+        </div>
       </div>
+
+      <p>
+        <a href=" http://www.vlir.be/personeelsstatistieken%20rapport" target="_blank">Bekijk hier de personeelsstatistieken van de VLIR.</a>
+      </p>
+
     </div>
+    
+    
+    
   `,
   styles: []
 })
